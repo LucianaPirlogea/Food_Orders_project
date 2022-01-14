1d4d43ee69b11f1c4be4abb8045bae740f632f08
LucianaPirlogea <luciapirlogea@yahoo.com>
2022-01-14 20:03:04 +0200

    Add project files.

---
+++ Food_Orders/Data/FoodOrdersContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Food_Orders.Data
+{
+    public class FoodOrdersContext: DbContext
+    {
+        public FoodOrdersContext(DbContextOptions<FoodOrdersContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+    
+}
